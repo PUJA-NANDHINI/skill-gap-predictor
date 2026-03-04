cf048d416b9555435ca33c1aa5b6cbfafbc7a82d
Puja Nandhini <pujanandhini2702@gmail.com>
2026-03-04 21:49:59 +0530

    debug test

--- web/script.js
+++ web/script.js
@@ -1,21 +1,21 @@
 async function predict() {
 
     const sequence = [
-        Number(document.getElementById("w1_ph").value),
-        Number(document.getElementById("w1_as").value),
-        Number(document.getElementById("w1_ec").value),
-        Number(document.getElementById("w1_qs").value),
+    Number(document.getElementById("w1_ph").value),
+    Number(document.getElementById("w1_as").value),
+    Number(document.getElementById("w1_ec").value),
+    Number(document.getElementById("w1_qs").value) / 10,
 
-        Number(document.getElementById("w2_ph").value),
-        Number(document.getElementById("w2_as").value),
-        Number(document.getElementById("w2_ec").value),
-        Number(document.getElementById("w2_qs").value),
+    Number(document.getElementById("w2_ph").value),
+    Number(document.getElementById("w2_as").value),
+    Number(document.getElementById("w2_ec").value),
+    Number(document.getElementById("w2_qs").value) / 10,
 
-        Number(document.getElementById("w3_ph").value),
-        Number(document.getElementById("w3_as").value),
-        Number(document.getElementById("w3_ec").value),
-        Number(document.getElementById("w3_qs").value)
-    ];
+    Number(document.getElementById("w3_ph").value),
+    Number(document.getElementById("w3_as").value),
+    Number(document.getElementById("w3_ec").value),
+    Number(document.getElementById("w3_qs").value) / 10
+];
 
     if (sequence.some(isNaN)) {
         alert("Please fill all fields properly.");
@@ -32,7 +32,7 @@ async function predict() {
         });
 
         const data = await response.json();
-        const score = data.predicted_quiz_score.toFixed(2);
+        const score = (data.predicted_quiz_score * 10).toFixed(2);
 
         document.getElementById("result").innerHTML =
              "<strong>Predicted Quiz Score:</strong> " + score + "<br><br>" +
